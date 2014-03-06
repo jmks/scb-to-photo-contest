f62f590c23d77d4f1277083ca5bde51dc2164c9f
jmks <jason.schweier@gmail.com>
2014-03-05 19:00:05 -0500

    edited aws uploading. still not redirecting on success

--- app/controllers/photo_entry_controller.rb
+++ app/controllers/photo_entry_controller.rb
@@ -2,27 +2,28 @@ class PhotoEntryController < ApplicationController
   before_filter :authenticate_contestant!
 
   def new
-    redirect_to contestant_index_path(@contestant) and return unless params[:photo_id]
+    redirect_to contestant_index_path and return unless params[:photo_id]
 
     @contestant = current_contestant
     @photo      = Photo.find(params[:photo_id])
 
-    redirect_to contestant_index_path(@contestant) and return unless @photo
+    # TODO flash messages
+    unless @photo && @photo.owner.id == @contestant.id
+      redirect_to contestant_index_path and return
+    end
     
     @uploader = Photo.new.photo
-    @uploader.success_action_redirect = photo_entry_path
+    @uploader.success_action_redirect = photo_entry_path id: @photo.id
   end
 
   # not from a post!
   def create
-    if params[:key].nil? || params[:photo_id].nil?
+    unless params[:key] && params[:photo_id]
       flash[:notice] = "Invalid photo creation request. Please reupload your photo."
       redirect_to contestant_index_path and return
     end
 
     photo_aws_key = params[:key]
     photo_id      = params[:photo_id]
-
-    
   end
 end
