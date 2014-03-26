7642f6ec4a8ca4e85a179b80a3d1992cc8f45a34
jmks <jason.schweier@gmail.com>
2014-03-26 09:33:40 -0400

    more restyling of workflow and minor annoyances

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  # move to ContestRules
   CATEGORIES = [ :flora, :fauna, :landscapes ]
   
   Registration = [ :submitted, :uploaded, :printed, :confirmed ]
@@ -11,9 +12,6 @@ class Photo
     :printed   => 'Awaiting Printed Copy',
     :confirmed => 'Registration Complete'
   }
-
-  #mount_uploader :photo, PhotoEntryUploader
-  #field :photo
 
   # photo details fields
   field :title
@@ -39,21 +37,20 @@ class Photo
   field :original_filename # not useful
 
   # thumbnail related -- will update
-  field :thumbnail_url
+  field :thumbnail_url # not useful
 
   field :thumbnail_xs_url
   field :thumbnail_sm_url
   field :thumbnail_lg_url
 
   # registration details
-  field :printed, type: Boolean, default: false
   field :order_number
 
-  field :votes,       type: Integer, default: 0
-  field :views,       type: Integer, default: 0
+  field :votes, type: Integer, default: 0
+  field :views, type: Integer, default: 0
 
   belongs_to :owner, :class_name => 'Contestant', :inverse_of => :entries
-  validates :owner, presence: true
+  validates  :owner, presence: true
   
   # scopes
   scope :landscapes, ->{ where(:category => "landscapes") }
@@ -95,7 +92,7 @@ class Photo
   def registration_status
     if !original_url
       :submitted
-    elsif printed
+    elsif order_number
       :confirmed
     else
       :uploaded
