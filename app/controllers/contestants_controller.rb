c331832ea2782513a0e59e048e952de6c4908fb2
jmks <jason.schweier@gmail.com>
2014-04-02 19:44:46 -0400

    added alert for contestants with incomplete submissions

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -7,9 +7,14 @@ class ContestantsController < ApplicationController
     votes             = Vote.where(_id: request.remote_ip).first
     @votes_left_today = ContestRules::VOTES_PER_DAY_PER_IP - (votes ? votes.votes_today : 0)
 
+    # can be removed
     if session[:invalid_photo_order_numbers]
       @invalid_photo_order_numbers = session[:invalid_photo_order_numbers]
       session.delete :invalid_photo_order_numbers
+    end
+
+    if @contestant.entries.select { |p| p.registration_status != :confirmed }.any?
+      flash.now[:alert] = "You currently have photo entries that are not completed. Please check the ACTIONS REQUIRED column in YOUR SUBMISSIONS"
     end
   end
 end
