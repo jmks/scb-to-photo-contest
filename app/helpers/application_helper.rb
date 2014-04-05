c838245f8251f843a92e25f3014485637fabaaba
jmks <jason.schweier@gmail.com>
2014-04-05 14:21:18 -0400

    added full page image support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
     # better?
     [current_page?(about_path), current_page?(judges_path), current_page?(rules_path)].any?
   end
+
+  def background_class
+    if on_photo_step?
+      'rouge2005'
+    else
+      nil
+    end
+  end
+
+  def on_photo_step?
+    [current_page?(new_photo_path), current_page?(new_photo_entry_path), current_page?(print_photo_entry_path), current_page?(verify_photo_entry_path), current_page?(share_photo_entry_path)].any?
+  end
 end
