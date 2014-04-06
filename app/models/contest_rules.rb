c6f14b58d9ff550b94892dd0161694ddf5148652
jmks <jason.schweier@gmail.com>
2014-04-06 16:11:50 -0400

    updated contest rules

--- app/models/contest_rules.rb
+++ app/models/contest_rules.rb
@@ -1,9 +1,10 @@
 class ContestRules
-    CONTEST_OPENS  = DateTime.new(2014, 3, 24)
-    CONTEST_CLOSES = DateTime.new(2014, 4, 18)
+    CONTEST_OPENS  = DateTime.new(2014, 4, 14)
+    CONTEST_CLOSES = DateTime.new(2014, 5, 14)
 
-    JUDGING_OPENS  = DateTime.new(2014, 4, 21)
-    JUDGING_CLOSES = DateTime.new(2014, 4, 21)
+    JUDGING_OPENS  = DateTime.new(2014, 5, 15)
+    JUDGING_CLOSES = DateTime.new(2014, 4, 22)
 
     VOTES_PER_DAY_PER_IP = 5
+    ENTRIES_PER_CONTESTANT = 5
 end
