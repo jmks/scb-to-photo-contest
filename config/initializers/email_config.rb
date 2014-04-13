da9d4428e082b0e4cf5dec48367f1aaee34b86c4
jmks <jason.schweier@gmail.com>
2014-04-13 17:06:11 -0400

    new gmail config for production

--- config/initializers/email_config.rb
+++ config/initializers/email_config.rb
@@ -3,7 +3,8 @@ ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :address              => 'smtp.gmail.com',
     :port                 => 587,
-    :authentication       => 'plain',
+    :domain               => 'scbtorontophotos.com',
+    :authentication       => :login,
     :user_name            => ENV["GMAIL_USERNAME"],
     :password             => ENV["GMAIL_PASSWORD"]
 }
