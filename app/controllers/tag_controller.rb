50ca3104927a589c3204ebb3564619b0b127d30c
jmks <jason.schweier@gmail.com>
2014-04-13 12:02:55 -0400

    escaped regex in tag search and made case insensitive

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -2,7 +2,7 @@ class TagController < ApplicationController
 
   def index
     if params[:q]
-        render :json => Tag.where(name: /#{params[:q]}/).map(&:name)
+        render :json => Tag.where(name: /#{Regexp.escape(params[:q])}/i).map(&:name)
     else
         render :json => Tag.get_tags
     end
