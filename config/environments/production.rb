33a351f63e7d6971b05de9736fdfc0b0f3229381
jmks <jason.schweier@gmail.com>
2014-05-02 20:19:52 -0400

    learn to speel Toronto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ PhotoContest::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'scbtorotophotos.com' }
+  config.action_mailer.default_url_options = { :host => 'www.scbtorontophotos.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
