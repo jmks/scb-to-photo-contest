2f12b13efb71ebe9fc305cfff47517be6ab8c946
jmks <jason.schweier@gmail.com>
2015-05-20 10:16:44 -0400

    removed erroneous and unused helper method

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,19 +1,11 @@
 module PhotosHelper
-  
-  # TODO: remove. hope this isn't used!
-  def months
-    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Auguflash.now[:alert] = st', 'September', 'October', 'November', 'December']
-  end
-
-  # display helpers
-
   def display_photo_month
     (@photo.photo_date? && @photo.photo_date.split(' ').first) || Date.today.strftime('%B')
   end
 
   def display_photo_year
     (@photo.photo_date? && @photo.photo_date.split(' ').last) || Date.today.strftime('%Y')
-  end  
+  end
 
   def photographer_link
     content_tag(:a, content_tag(:strong, @photo.owner.public_name), href: photos_path(contestant_id: @photo.owner.id))
@@ -28,7 +20,7 @@ module PhotosHelper
   end
 
   def display property, default
-    if @photo.respond_to?("#{property}?".to_sym) and @photo.send(property.to_sym)
+    if @photo.respond_to?("#{property}?".to_sym) && @photo.send(property.to_sym)
       @photo.send(property.to_sym)
     else
       default
