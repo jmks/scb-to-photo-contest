4417b0b418cbcd94c96b3bac1c3c3ba0d988ef9b
jmks <jason.schweier@gmail.com>
2015-05-25 15:50:39 -0400

    fixed photo factory association to a contest

--- spec/factories/photo_factory.rb
+++ spec/factories/photo_factory.rb
@@ -1,10 +1,11 @@
-FactoryGirl.define do 
-  factory :photo do 
+FactoryGirl.define do
+  factory :photo do
     sequence :title do |n|
       "Photo title #{n}"
     end
     category { Photo::CATEGORIES.sample }
     association :owner, factory: :contestant
+    association :contest, factory: :contest
     tags %w{tag1 tag2 tag3 tag4}.sample(2)
   end
 end
