582426ca1310da3a77c54749f78e7f0d9b0553c9
jmks <jason.schweier@gmail.com>
2015-11-19 16:02:05 -0500

    added better error handling for resque initialization

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,6 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-if Rails.env.production?
-    Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+begin
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+rescue URI::InvalidURIError
+  Rails.logger.error "The env var REDISTOGO_URL is invalid"
 end
