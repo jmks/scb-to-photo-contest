7ce48df3361d73591b58ee1d78e14c541774008a
jmks <jason.schweier@gmail.com>
2015-11-22 15:21:05 -0500

    fixed type and update string quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
     [root_path, prizes_path].map { |path| current_page? path }.any?
   end
 
-  # to highlight 'about' dropdown in navbar
+  # to highlight "about" dropdown in navbar
   def about_page?
     [about_path, contest_path, judges_path, rules_path].each do |path|
       return true if current_page? path
@@ -29,32 +29,32 @@ module ApplicationHelper
     false
   end
 
-  # to higlight 'gallery' in navbar
+  # to highlight "gallery" in navbar
   def gallery_path?
-    params[:controller] == 'photos' && params[:action] == 'index'
+    params[:controller] == "photos" && params[:action] == "index"
   end
 
   def navbar_class
     if current_page? root_path
-      'navbar-inverse'
+      "navbar-inverse"
     else
-      ''
+      ""
     end
   end
 
   def background_class
     if on_photo_step?
-      'rouge2005'
+      "rouge2005"
     elsif current_page?(about_path) || current_page?(contest_path) || current_page?(rules_path)
-      'grey-bg' # light blue
+      "grey-bg" # light blue
     elsif contestant_admin_path?
-      'fieldsite'
+      "fieldsite"
     elsif current_page?(judges_path) || current_page?(prizes_path)
-      'fieldsite'
+      "fieldsite"
     elsif current_page?(photos_path) || current_page?(contestant_index_path)
-      'light-grey-bg' # light-white
+      "light-grey-bg" # light-white
     elsif current_page? root_path
-      'home'
+      "home"
     else
       nil
     end
@@ -65,19 +65,19 @@ module ApplicationHelper
       return true if current_page? path
     end
     # photo#create post path
-    params[:controller] == 'photos' && params[:action] == 'create'
+    params[:controller] == "photos" && params[:action] == "create"
   end
 
   def contestant_admin_path?
     devise_controller? && params[:action] != :index
   end
 
-  def svg_png_fallback svg_path, html_attrs, fallback_data_attr='fallback'
-    attributes = html_attrs.merge 'src' => asset_path(svg_path), 'data' => { fallback_data_attr => asset_path(svg_path.gsub(/svg\Z/, 'png')) }
-    tag('img', attributes)
+  def svg_png_fallback svg_path, html_attrs, fallback_data_attr="fallback"
+    attributes = html_attrs.merge "src" => asset_path(svg_path), "data" => { fallback_data_attr => asset_path(svg_path.gsub(/svg\Z/, "png")) }
+    tag("img", attributes)
   end
 
   def long_date date
-    date.strftime('%B %-d, %Y')
+    date.strftime("%B %-d, %Y")
   end
 end
